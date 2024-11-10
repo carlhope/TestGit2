d07cd755d88292c60bc7eae5a62f3ee64e7ef673
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-11-10 16:13:42 +0000

    updated

--- TestGit2/Fruit.cs
+++ TestGit2/Fruit.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace TestGit2
 {
-    internal class Fruit
+    public class Fruit
     {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public Fruit(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
     }
 }
