fb6c2c8b809ca6a2d2eb159aaac4bc9f8c2e80c5
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-11-10 16:00:11 +0000

    added Person and World

--- TestGit2/Program.cs
+++ TestGit2/Program.cs
@@ -11,6 +11,14 @@
             string test2 = String.Join("",testarray);
             Console.WriteLine(test2);
             //hello
+            Person person = new Person("John", 30);
+            Person.PrintPerson(person);
+            //string tes2 = test.Join("", testarray);
+            Person Jane = new Person("Jane", 25);
+            Person.PrintPerson(Jane);
+            Console.WriteLine(Person.Count);
+            Jane.Dispose();
+            Console.WriteLine(Person.Count);
         }
     }
 }
